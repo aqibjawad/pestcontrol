0f746df7e61b48cb52df58533cfb20aa6a2953bd
Aqib Jawad <aqibjawad123@gmail.com>
2024-07-08 12:26:39 +0500

    statement
    account statement done

--- app/accountStatment/page.jsx
+++ app/accountStatment/page.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React from "react";
 
-const Page =()=>{
-    return(
+import AccountDetails from "./accountDetails";
+import StatmentAccount from "./statment";
+import AmountRegards from "./amountRegard";
 
-        <div>
-            Test
-        </div>
-    )
-}
+const Page = () => {
+  return (
+    <div>
+      <div className="flex justify-between" style={{ padding: "34px" }}>
+        <img src="/leftimage.png" style={{ width: "100%" }} />
+        <img src="/rightimage.png" style={{ width: "100%" }} />
+      </div>
+        
+        <AccountDetails />
+        <StatmentAccount />
+        <AmountRegards />
+
+      <div className="flex justify-between" style={{ padding: "34px" }}>
+        <img src="/leftDown.png" style={{width:"100%"}} />
+        <img src="/rightDown.png" style={{width:"100%"}} />
+      </div>
+    </div>
+  );
+};
 
 export default Page;
