b1b32fe4116e93d8038b42af04bdcd6c0f4c9ba1
Aqib Jawad <aqibjawad123@gmail.com>
2024-09-13 17:46:14 +0500

    add purchase button added in settings

--- app/setting/page.jsx
+++ app/setting/page.jsx
@@ -52,6 +52,12 @@ const Page = () => {
           <Link href="/operations/addExpense">Add Vehicle Expense</Link>
         </div>
       </Grid>
+
+      <Grid item xs={12} sm={6} md={4} lg={3}>
+        <div className={styles.addProd}>
+          <Link href="/account/addPurchase">Add Purchase Order</Link>
+        </div>
+      </Grid>
     </Grid>
   );
 };
