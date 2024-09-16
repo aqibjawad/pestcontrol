59c7a156f4008505f169f3d8b033d6bbdea7c8b7
Aqib Jawad <aqibjawad123@gmail.com>
2024-09-16 18:20:59 +0500

    all employee and employee detail page done

--- app/hr/employeeDetails/page.jsx
+++ app/hr/employeeDetails/page.jsx
@@ -1,18 +1,229 @@
-import React from "react";
-import PersonalDetails from "./personlDetails";
-import Identification from "./identification";
-import Insurance from "./insurrance";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import Contact from "./contact";
 
-const Page =()=>{
-    return(
-        <div>
-            <PersonalDetails />
-            <Identification />
-            <Insurance />
-            <Contact />
+import styles from "../../../styles/personalDetails.module.css";
+
+import APICall from "@/networkUtil/APICall";
+import { getAllEmpoyesUrl } from "@/networkUtil/Constants";
+
+import { useSearchParams } from "next/navigation";
+
+const Page = () => {
+  const api = new APICall();
+
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
+
+  const [fetchingData, setFetchingData] = useState(false);
+  const [employeeList, setEmployeeList] = useState([]);
+
+  useEffect(() => {
+    getAllEmployees();
+  }, []);
+
+  const getAllEmployees = async () => {
+    setFetchingData(true);
+    try {
+      const response = await api.getDataWithToken(`${getAllEmpoyesUrl}/${id}`);
+      setEmployeeList(response.data);
+    } catch (error) {
+      console.error("Error fetching vehicles:", error);
+    } finally {
+      setFetchingData(false);
+    }
+  };
+
+  return (
+    <div>
+      {/* Personal Inofrmation */}
+      <div className={styles.personalDetailsContainer}>
+        <div className={styles.imageContainer}>
+          <img
+            className={styles.personalImage}
+            src={employeeList?.employee?.profile_image}
+            alt="Person"
+          />
         </div>
-    )
-}
+        <div className={styles.personalName}> {employeeList.name} </div>
+
+        <div className={styles.personalContainer}>
+          <div className={styles.personalHead}>Personal Image</div>
+
+          <div className={styles.tableContainer}>
+            <table className={styles.table}>
+              <thead>
+                <tr>
+                  <th>Email</th>
+                  <th>Contact</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td> {employeeList.email} </td>
+                  <td> {employeeList?.employee?.phone_number} </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+
+      {/* Identification Inofrmation */}
+      <div>
+        <div className={styles.personalContainer}>
+          <div className={styles.personalHead}> Identification </div>
+
+          <div className={styles.tableContainer}>
+            <table className={styles.table}>
+              <thead>
+                <tr>
+                  <th>EID No</th>
+                  <th>Start Date</th>
+                  <th>Expiry Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td> {employeeList?.employee?.eid_no} </td>
+                  <td> {employeeList?.employee?.eid_start} </td>
+                  <td> {employeeList?.employee?.eid_expiry} </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        <div className={styles.personalContainer}>
+          <div className={styles.personalHead}> Passport Details </div>
+
+          <div className={styles.tableContainer}>
+            <table className={styles.table}>
+              <thead>
+                <tr>
+                  <th>Passport No</th>
+                  <th>Start Date</th>
+                  <th>Expiry Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td> {employeeList?.employee?.passport_no} </td>
+                  <td> {employeeList?.employee?.passport_start} </td>
+                  <td> {employeeList?.employee?.passport_expiry} </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+
+      {/* Insurance */}
+      <div>
+        <div className={styles.personalContainer}>
+          <div className={styles.personalHead}> Health Insurrance </div>
+
+          <div className={styles.tableContainer}>
+            <table className={styles.table}>
+              <thead>
+                <tr>
+                  <th>Health Insurrance</th>
+                  <th>Start Date</th>
+                  <th>Expiry Date</th>
+                  <th> DM Card</th>
+                  <th> Card Start </th>
+                  <th> Card Expiry </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td> {employeeList?.employee?.hi_status} </td>
+                  <td> {employeeList?.employee?.hi_start} </td>
+                  <td> {employeeList?.employee?.hi_expiry} </td>
+                  <td> {employeeList?.employee?.dm_card} </td>
+                  <td> {employeeList?.employee?.dm_start} </td>
+                  <td> {employeeList?.employee?.dm_expiry} </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        <div className={styles.personalContainer}>
+          <div className={styles.personalHead}> Unemployment Insurance </div>
+
+          <div className={styles.tableContainer}>
+            <table className={styles.table}>
+              <thead>
+                <tr>
+                  <th> Unemployment Insurance status</th>
+                  <th> Start Date </th>
+                  <th> Expiry Date </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td> {employeeList?.employee?.ui_status} </td>
+                  <td> {employeeList?.employee?.ui_start} </td>
+                  <td> {employeeList?.employee?.ui_expiry} </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+
+      {/* Contact */}
+      <div>
+        <div className={styles.personalContainer}>
+          <div className={styles.personalHead}> Emergency Contact </div>
+
+          <div className={styles.tableContainer}>
+            <table className={styles.table}>
+              <thead>
+                <tr>
+                  <th> Name </th>
+                  <th> Relation </th>
+                  <th> Contact </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td> {employeeList?.employee?.relative_name} </td>
+                  <td> {employeeList?.employee?.relation} </td>
+                  <td> {employeeList?.employee?.emergency_contact} </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        <div className={styles.personalContainer}>
+          <div className={styles.personalHead}> Financial Information </div>
+
+          <div className={styles.tableContainer}>
+            <table className={styles.table}>
+              <thead>
+                <tr>
+                  <th> Basic Salary </th>
+                  <th> Allowance </th>
+                  <th> Total Salary </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td> {employeeList?.employee?.basic_salary} </td>
+                  <td> {employeeList?.employee?.allowance} </td>
+                  <td> {employeeList?.employee?.total_salary} </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Page;
--- app/operations/viewEmployees/page.jsx
+++ app/operations/viewEmployees/page.jsx
@@ -1,11 +1,119 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
 
-const Page =()=>{
-    return(
-        <div>
-            test
+import tableStyles from "../../../styles/upcomingJobsStyles.module.css";
+import Link from "next/link";
+import SearchInput from "@/components/generic/SearchInput";
+import DateFilters from "../../../components/generic/DateFilters";
+
+import APICall from "@/networkUtil/APICall";
+import { getAllEmpoyesUrl } from "@/networkUtil/Constants";
+
+import { useSearchParams } from "next/navigation";
+
+const Page = () => {
+  const api = new APICall();
+
+  const [fetchingData, setFetchingData] = useState(false);
+  const [expenseList, setExpenseList] = useState([]);
+
+  useEffect(() => {
+    getAllExpenses();
+  }, []);
+
+  const getAllExpenses = async () => {
+    setFetchingData(true);
+    try {
+      const response = await api.getDataWithToken(`${getAllEmpoyesUrl}`);
+      setExpenseList(response.data);
+    } catch (error) {
+      console.error("Error fetching vehicles:", error);
+    } finally {
+      setFetchingData(false);
+    }
+  };
+
+  const listServiceTable = () => {
+    return (
+      <div className={tableStyles.tableContainer}>
+        <table className="min-w-full bg-white mt-5">
+          <thead>
+            <tr>
+              <th className="py-5 px-4 border-b border-gray-200 text-left">
+                Sr.
+              </th>
+              <th className="py-2 px-4 border-b border-gray-200 text-left">
+                Employee Name
+              </th>
+              <th className="py-2 px-4 border-b border-gray-200 text-left">
+                Employee Designation
+              </th>
+              <th className="py-2 px-4 border-b border-gray-200 text-left">
+                Contact
+              </th>
+              <th className="py-2 px-4 border-b border-gray-200 text-left">
+                Status
+              </th>
+              <th className="py-2 px-4 border-b border-gray-200 text-left">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {expenseList.map((row, index) => (
+              <tr key={index} className="border-b border-gray-200">
+                <td className="py-5 px-4">{index + 1}</td>
+                <td className="py-2 px-4">
+                  <div className={tableStyles.clientContact}>{row.name}</div>
+                </td>
+                <td className="py-2 px-4">
+                  <div className={tableStyles.clientContact}>{row.email}</div>
+                </td>
+                <td className="py-2 px-4">
+                  <div className={tableStyles.clientContact}>
+                    {row.contact || "null"}
+                  </div>
+                </td>
+                <td className="py-2 px-4">
+                  <div className={tableStyles.clientContact}>
+                    {row.is_active === 1 ? "Inactive" : "Active"}
+                  </div>
+                </td>
+                <td className="py-2 px-4">
+                  <div className={tableStyles.clientContact}>
+                    <Link href={`/hr/employeeDetails?id=${row.id}`}>
+                      <span className="text-blue-600 hover:text-blue-800">
+                        View Details
+                      </span>
+                    </Link>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <div className="flex">
+        <div className="flex flex-grow">
+          <div className="pageTitle">{"All Emplyees"}</div>
         </div>
-    )
-}
+        <div className="flex">
+          <div className="mr-10">
+            <SearchInput />
+          </div>
 
-export default Page
+          <DateFilters />
+        </div>
+      </div>
+
+      {listServiceTable()}
+    </div>
+  );
+};
+
+export default Page;
