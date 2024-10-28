01b0e054e861c0cde6cd88c2cb406300990f735f
Aqib Jawad <aqibjawad123@gmail.com>
2024-10-28 16:32:54 +0500

    accountant view done

--- app/accountant/viewAccountant/page.jsx
+++ app/accountant/viewAccountant/page.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import {
   Table,
   TableBody,
@@ -7,33 +9,127 @@ import {
   TableHead,
   TableRow,
   Paper,
+  CircularProgress,
 } from "@mui/material";
+import SearchInput from "@/components/generic/SearchInput";
+import APICall from "@/networkUtil/APICall";
+import { clients } from "@/networkUtil/Constants";
+
+import Link from "next/link";
 
 const Page = () => {
+  const api = new APICall();
+
+  const [fetchingData, setFetchingData] = useState(false);
+  const [quoteList, setQuoteList] = useState([]);
+
+  useEffect(() => {
+    getAllQuotes();
+  }, []);
+
+  const getAllQuotes = async () => {
+    setFetchingData(true);
+    try {
+      const response = await api.getDataWithToken(`${clients}`);
+      setQuoteList(response.data);
+    } catch (error) {
+      console.error("Error fetching quotes:", error);
+    } finally {
+      setFetchingData(false);
+    }
+  };
+
+  const listServiceTable = () => {
+    return (
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Sr No</TableCell>
+              <TableCell>Date</TableCell>
+              <TableCell>Client Name</TableCell>
+              <TableCell>Recieved By</TableCell>
+              <TableCell>Amount</TableCell>
+              <TableCell>View Details</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {fetchingData ? (
+              <TableRow>
+                <TableCell colSpan={7} style={{ textAlign: "center" }}>
+                  <CircularProgress />
+                </TableCell>
+              </TableRow>
+            ) : (
+              quoteList.map((row, index) => (
+                <TableRow key={index}>
+                  <TableCell>{index + 1}</TableCell>
+                  <TableCell>
+                    {new Date(row.created_at).toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })}
+                  </TableCell>
+                  <TableCell>{row?.name}</TableCell>
+                  <TableCell>{row?.client?.referencable?.name}</TableCell>
+                  <TableCell>{row?.received_amt}</TableCell>
+                  <TableCell>
+                    <Link href={`/customerLedger?id=${row.id}`}>
+                      <span className="text-blue-600 hover:text-blue-800">
+                        View Details
+                      </span>
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              ))
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  };
 
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Sr No</TableCell>
-            <TableCell>Date</TableCell>
-            <TableCell>Client Name</TableCell>
-            <TableCell>Recieved By</TableCell>
-            <TableCell>Amount</TableCell>
-            <TableCell>View Details</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableCell> 1 </TableCell>
-          <TableCell> October 28, 2024 </TableCell>
-          <TableCell> Aqib Jawad </TableCell>
-          <TableCell> Shahbaz Ali </TableCell>
-          <TableCell> 2800 </TableCell>
-          <TableCell> View Details </TableCell>
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div>
+      <div style={{ padding: "30px", borderRadius: "10px" }}>
+        <div
+          style={{ fontSize: "20px", fontWeight: "600", marginBottom: "-4rem" }}
+        >
+          All Clients Ledger Details
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            marginTop: "2rem",
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: "#32A92E",
+              color: "white",
+              fontWeight: "600",
+              fontSize: "16px",
+              height: "44px",
+              width: "202px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginLeft: "1rem",
+              padding: "12px 16px",
+              borderRadius: "10px",
+            }}
+          >
+            Date
+          </div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-12 gap-4">
+        <div className="col-span-12">{listServiceTable()}</div>
+      </div>
+    </div>
   );
 };
 
