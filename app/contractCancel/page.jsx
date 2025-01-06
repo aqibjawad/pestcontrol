6a23050b49c56a066a1e071fbdf8847c896b97c0
Aqib Jawad <aqibjawad123@gmail.com>
2025-01-06 16:33:59 +0500

    Global CSS added

--- app/contractCancel/page.jsx
+++ app/contractCancel/page.jsx
@@ -17,6 +17,7 @@ import {
   Box,
   Typography,
   Skeleton,
+  Paper
 } from "@mui/material";
 
 import { quotation } from "@/networkUtil/Constants";
@@ -26,6 +27,8 @@ import APICall from "@/networkUtil/APICall";
 import InputWithTitle3 from "@/components/generic/InputWithTitle3";
 
 import Swal from "sweetalert2";
+
+import styles from "../../styles/viewQuote.module.css";
 
 const getIdFromUrl = (url) => {
   const parts = url.split("?");
@@ -129,82 +132,175 @@ const Page = () => {
     // }
   };
 
+  const formatDate = (dateString) => {
+    if (!dateString) return "N/A"; // Handle no date case
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    const date = new Date(dateString);
+    return date.toLocaleDateString(undefined, options); // Formats to "Month Day, Year"
+  };
+
   return (
     <Layout>
       <div className="text-red-500 text-center">Contract Cancel</div>
 
-      <TableContainer>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell> Sr No </TableCell>
-              <TableCell> Job Title </TableCell>
-              <TableCell> Client Name </TableCell>
-              <TableCell> Firm Name </TableCell>
-              <TableCell> Completed </TableCell>
-              <TableCell> Complete Time </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {fetchingData ? (
-              // Show Skeletons while fetching data
-              [...Array(5)].map((_, index) => (
-                <TableRow key={index}>
+      <Grid container spacing={3}>
+        <Grid item lg={6} xs={12} sm={6} md={4}>
+          <TableContainer sx={{ mt: 6 }}>
+            <Table sx={{ borderCollapse: "collapse" }}>
+              <TableBody>
+                <TableRow sx={{ border: "none" }}>
                   <TableCell>
-                    <Skeleton variant="text" />
+                    <strong> Customer:</strong>
+                  </TableCell>
+                  <TableCell> {invoiceList?.user?.name} </TableCell>
+                </TableRow>
+
+                <TableRow sx={{ border: "none" }}>
+                  <TableCell>
+                    <strong> Reference:</strong>
                   </TableCell>
                   <TableCell>
-                    <Skeleton variant="text" />
-                  </TableCell>
-                  <TableCell>
-                    <Skeleton variant="text" />
-                  </TableCell>
-                  <TableCell>
-                    <Skeleton variant="text" />
-                  </TableCell>
-                  <TableCell>
-                    <Skeleton variant="text" />
-                  </TableCell>
-                  <TableCell>
-                    <Skeleton variant="text" />
+                    {" "}
+                    {invoiceList?.user?.client?.referencable?.name}{" "}
                   </TableCell>
                 </TableRow>
-              ))
-            ) : invoiceJobList.length === 0 ? (
-              // Show a "No data" message if the job list is empty
+
+                <TableRow sx={{ border: "none" }}>
+                  <TableCell>
+                    <strong> Job title: </strong>
+                  </TableCell>
+                  <TableCell> {invoiceList?.quote_title} </TableCell>
+                </TableRow>
+
+                <TableRow sx={{ border: "none" }}>
+                  <TableCell>
+                    <strong> Country: </strong>
+                  </TableCell>
+                  <TableCell> UAE </TableCell>
+                </TableRow>
+
+                <TableRow sx={{ border: "none" }}>
+                  <TableCell>
+                    <strong> Priority: </strong>
+                  </TableCell>
+                  <TableCell> High </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+
+        <Grid item lg={6} xs={12} sm={6} md={4}>
+          <TableContainer sx={{ mt: 6 }}>
+            <Table sx={{ borderCollapse: "collapse" }}>
+              <TableBody>
+                <TableRow sx={{ border: "none" }}>
+                  <TableCell>
+                    <strong> Job type : </strong>
+                  </TableCell>
+                  <TableCell> Reoccuring </TableCell>
+                </TableRow>
+
+                <TableRow sx={{ border: "none" }}>
+                  <TableCell>
+                    <strong> Duration: </strong>
+                  </TableCell>
+                  <TableCell> {invoiceList?.duration_in_months} </TableCell>
+                </TableRow>
+
+                <TableRow sx={{ border: "none" }}>
+                  <TableCell>
+                    <strong> TRN: </strong>
+                  </TableCell>
+                  <TableCell> {invoiceList?.trn} </TableCell>
+                </TableRow>
+
+                <TableRow sx={{ border: "none" }}>
+                  <TableCell>
+                    <strong> Date: </strong>
+                  </TableCell>
+                  <TableCell> {formatDate(invoiceList?.updated_at)} </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+      </Grid>
+
+      <div className={styles.clientRecord}>
+        <div className={styles.clientHead}>Service Product</div>
+
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+            <TableHead className={styles.tableHead}>
               <TableRow>
-                <TableCell colSpan={6} align="center">
-                  No job is started
-                </TableCell>
+                <TableCell align="center"> Sr # </TableCell>
+                <TableCell align="center"> Client Name </TableCell>
+                <TableCell align="center"> Firm Name </TableCell>
+                <TableCell align="center"> Completed </TableCell>
+                <TableCell align="center"> Complete Time </TableCell>
               </TableRow>
-            ) : (
-              // Map through the data when it is loaded
-              invoiceJobList.map((job, index) => (
-                <TableRow key={job.id || index}>
-                  <TableCell>{index + 1}</TableCell>
-                  <TableCell>{job.job_title}</TableCell>
-                  <TableCell>{job.user?.name}</TableCell>
-                  <TableCell>{job.user?.client?.firm_name}</TableCell>
-                  <TableCell>
-                    {job.is_completed === 0
-                      ? "Not Started"
-                      : job.is_completed === 1
-                      ? "Complete"
-                      : job.is_completed === 2
-                      ? "In Process"
-                      : "Unknown"}
-                  </TableCell>
-                  <TableCell>
-                    {job.job_end_time
-                      ? new Date(job.job_end_time).toISOString().split("T")[0]
-                      : ""}
+            </TableHead>
+            <TableBody>
+              {fetchingData ? (
+                // Show Skeletons while fetching data
+                [...Array(5)].map((_, index) => (
+                  <TableRow key={index}>
+                    <TableCell>
+                      <Skeleton variant="text" />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton variant="text" />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton variant="text" />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton variant="text" />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton variant="text" />
+                    </TableCell>
+                  </TableRow>
+                ))
+              ) : invoiceJobList.length === 0 ? (
+                // Show a "No data" message if the job list is empty
+                <TableRow>
+                  <TableCell colSpan={6} align="center">
+                    No job is started
                   </TableCell>
                 </TableRow>
-              ))
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
+              ) : (
+                // Map through the data when it is loaded
+                invoiceJobList.map((job, index) => (
+                  <TableRow
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    key={job.id || index}
+                  >
+                    <TableCell>{index + 1}</TableCell>
+                    <TableCell>{job.user?.name}</TableCell>
+                    <TableCell>{job.user?.client?.firm_name}</TableCell>
+                    <TableCell>
+                      {job.is_completed === 0
+                        ? "Not Started"
+                        : job.is_completed === 1
+                        ? "Complete"
+                        : job.is_completed === 2
+                        ? "In Process"
+                        : "Unknown"}
+                    </TableCell>
+                    <TableCell>
+                      {job.job_end_time
+                        ? new Date(job.job_end_time).toISOString().split("T")[0]
+                        : ""}
+                    </TableCell>
+                  </TableRow>
+                ))
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
 
       {/* Grid for Buttons */}
       <Grid container spacing={2} justifyContent="center" marginTop={2}>
